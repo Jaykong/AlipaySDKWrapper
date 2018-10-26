a1933c007521add61c7abcd65070049515674d2b
kongyunpeng2011@sina.com <kongyunpeng2011@sina.com>
2018-10-26 22:44:29 +0800

    Initial Commit

---
+++ AlipaySDKWrapper/AlipaySDKWrapper.h
@@ -0,0 +1,19 @@
+//
+//  AlipaySDKWrapper.h
+//  AlipaySDKWrapper
+//
+//  Created by Jay Kong on 2018/10/26.
+//  Copyright © 2018 disney. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AlipaySDKWrapper.
+FOUNDATION_EXPORT double AlipaySDKWrapperVersionNumber;
+
+//! Project version string for AlipaySDKWrapper.
+FOUNDATION_EXPORT const unsigned char AlipaySDKWrapperVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AlipaySDKWrapper/PublicHeader.h>
+
+
